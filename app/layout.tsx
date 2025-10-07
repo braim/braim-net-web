57582699a9768b22b862d4952f873d5b665e31df
braim <admin@auctionforecaster.com>
2025-10-07 22:28:04 +1000

    add next/third-parties

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,62 +1,54 @@
-import {Footer, Layout, Navbar} from 'nextra-theme-docs'
-import {Banner, Head} from 'nextra/components'
-import {getPageMap} from 'nextra/page-map'
-import 'nextra-theme-docs/style.css'
+import { Footer, Layout, Navbar } from "nextra-theme-docs";
+import { Banner, Head } from "nextra/components";
+import { getPageMap } from "nextra/page-map";
+import "nextra-theme-docs/style.css";
 import Link from "next/link";
+import { GoogleAnalytics } from "@next/third-parties/google";
 
 export const metadata = {
-    // Define your metadata here
-    // For more information on metadata API, see: https://nextjs.org/docs/app/building-your-application/optimizing/metadata
-}
+  // Define your metadata here
+  // For more information on metadata API, see: https://nextjs.org/docs/app/building-your-application/optimizing/metadata
+};
 
-const banner = <Banner storageKey="banner-under-construction-key">🚧 👷 This website is under construction 👷 🚧</Banner>
+const banner = <Banner storageKey="banner-under-construction-key">🚧 👷 This website is under construction 👷 🚧</Banner>;
 const navbar = (
-    <Navbar
-        logo={<img src="/images/general/logo.svg" alt="Logo" width={100} height={20}/>}
-        // ... Your additional navbar options
-    />
-)
-const footer = <Footer>{new Date().getFullYear()} © BRAIM.</Footer>
+  <Navbar
+    logo={<img src="/images/general/logo.svg" alt="Logo" width={100} height={20} />}
+    // ... Your additional navbar options
+  />
+);
+const footer = <Footer>{new Date().getFullYear()} © BRAIM.</Footer>;
 
-export default async function RootLayout({children}) {
-    return (
-        <html
-            // Not required, but good for SEO
-            lang="en"
-            // Required to be set
-            dir="ltr"
-            // Suggested by `next-themes` package https://github.com/pacocoursey/next-themes#with-app
-            suppressHydrationWarning
-        >
-        <Head
-            // ... Your additional head options
-        >
-            <link rel="shortcut icon" href="/images/general/icon.svg"/>
-      
+export default async function RootLayout({ children }) {
+  return (
+    <html
+      // Not required, but good for SEO
+      lang="en"
+      // Required to be set
+      dir="ltr"
+      // Suggested by `next-themes` package https://github.com/pacocoursey/next-themes#with-app
+      suppressHydrationWarning
+    >
+      <Head
+      // ... Your additional head options
+      >
+        <link rel="shortcut icon" href="/images/general/icon.svg" />
 
-            {/* Your additional tags should be passed as `children` of `<Head>` element */}
-        </Head>
-        <body>
+        {/* Your additional tags should be passed as `children` of `<Head>` element */}
+      </Head>
+      <body>
+        <GoogleAnalytics gaId="G-BLYQM4FJCT" />
         <Layout
-            banner={banner}
-            navbar={navbar}
-            pageMap={await getPageMap()}
-            docsRepositoryBase="https://github.com/braim/braim-net-web/tree/main"
-            footer={footer}
-            // ... Your additional layout options
+          banner={banner}
+          navbar={navbar}
+          pageMap={await getPageMap()}
+          docsRepositoryBase="https://github.com/braim/braim-net-web/tree/main"
+          footer={footer}
+          // ... Your additional layout options
         >
-            {children}
+          {children}
         </Layout>
-
-            <script async src="https://www.googletagmanager.com/gtag/js?id=G-BLYQM4FJCT"></script>
-            <script>
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-
-            gtag('config', 'G-BLYQM4FJCT');
-            </script>
-        </body>
-        </html>
-    )
+      </body>
+    </html>
+  );
 }
